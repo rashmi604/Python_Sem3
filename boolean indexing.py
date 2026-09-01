c72783b6f2bede8eab405b34cd46862ec226c8c5
Rashmi Rajput <rjay3261@gmail.com>
2026-09-01 15:30:40 +0530

    ahbdhb

--- boolean indexing.py
+++ boolean indexing.py
@@ -35,6 +35,78 @@ A=np.array([[1],[2],[3]])
 B=np.array([10,20,30])
 print(A+B)
 
+#Copy vs view
+import numpy as np
+arr=np.array([1,3,,4])
+view=arr.view()
+copy=arr.copy()
+arr[0]=100
+print(view)
+print(copy)
+
+#Marking
+arr=np.arange(20)
+mask=arr%2==0
+print(arr[mask])
+
+#File Handling
+arr=np.arange(10)
+np.save("data.npy",arr)
+
+#load
+data=np.load("data.npy")
+print(data)
+
+#Statics
+arr=np.array([1,2,3,4,5])
+
+print(np.mean(arr))
+print(np.median(arr))
+print(np.std(arr))
+print(np.var(arr))
+print(np.percentile(arr,50))
+
+#Universal Functons(ufunc)
+arr=np.array([1,4,9,16])
+
+print(np.sqrt(arr))
+print(np.square(arr))
+print(np.exp(arr))
+print(np.log(arr))
+print(np.sin(arr))
+print(np.cos(arr))
+
+#Advanced indexing
+arr=np.arange(20)
+print(arr)
+print(arr[[2,4,6]])
+
+#Fancy Indexing
+arr=np.array([
+    [1,2],
+    [3,4],
+    [5,6]
+])
+print(arr[[0,2]])
+
+#Broadcasting
+A=np.ones((3,3))
+print(A)
+B=np.array([1,2,3])
+print(A+B)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
