bccc9052ccfa5d49f473d41ff8b9491556265993
Rashmi Rajput <rjay3261@gmail.com>
2026-09-26 11:22:04 +0530

    plot

--- matplotlib.py
+++ matplotlib.py
@@ -46,3 +46,51 @@ plt.xlabel("Month")
 plt.ylabel("Sales")
 plt.legend(["2025", "2026"]) # Note: corrected from plt.legend("center") for proper labeling
 plt.show()
+
+
+import matplotlib.pyplot as plt
+students = ["Vikky","Rahul","Aman","Neh"]
+marks = [85,90,78,95]
+plt.bar(students, marks,width=0.6)
+plt.title("Student Marks")
+plt.xlabel("Student")
+plt.ylabel("Marks")
+plt.show()
+
+
+#Horizontal Bar Chart
+plt.barh(students,marks)
+plt.show()
+
+
+#Multiple Bar Chart
+students = ["A", "B", "C", "D"]
+python = [80,70,90,60]
+java = [70,75,85,80]
+import numpy as np
+x = np.arange(len(students))
+width = 0.35
+plt.bar(x - width/2, python, width, label="Python)
+plt.bar(x + width/2, java, width, label="Java")
+plt.xticks(x, students s)
+plt.legend()
+plt.show()
+
+
+import matplotlib.pyplot as plt
+subjects = ["AI","Pyhton","Java","SQL"]
+hours = [35,25,20,20]
+explode = [0.1,0,0,0]
+mycolours ["Red", "hotpink", "b", "#4CAF50"]
+plt.pie(hours,
+labels=subjects,
+autopct="%1.1f%%", #Percentage in Pie Chart
+explode=explode, #Explode Pie Chart
+shadow = True, #add shadow
+colors = mycolors #add colors
+)
+plt.title("Study Time")
+plt.legend()
+plt.show()
+
+
